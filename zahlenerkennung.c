3cad37e799f815421fb65979e02c613eac3fb56d
JanMeyfarth1999 <jan_meyfarth@teilnehmer.cdemy.de>
2026-07-16 15:24:14 +0200

    Version ohne zusatzaufgabe

--- zahlenerkennung.c
+++ zahlenerkennung.c
@@ -4,23 +4,26 @@ int main() {
     int zahl_a;
     int zahl_b;
     int zahl_c; 
-    int eingabe;
+    
 
         printf("Tippe 3 zahlen ein und lasse bestimmen welches die größte ist: "); 
-        scanf("%d" , &eingabe);  
+        scanf("%d" , &zahl_a);  
+        scanf("%d" , &zahl_b);
+        scanf("%d" , &zahl_c);
 
-        if(zahl_a > zahl_b & zahl_c) {
-            printf("zahl_a\n");
+
+        if(zahl_a > zahl_b && zahl_a > zahl_c) {
+            printf("%d\n", zahl_a);
 
         }
                                       
-        if(zahl_b > zahl_a & zahl_c) {
-            printf("zahl_b\n");
+        if(zahl_b > zahl_a && zahl_b > zahl_c) {
+            printf("%d\n", zahl_b);
 
         }
 
-        if(zahl_c > zahl_a & zahl_b) {
-            printf("zahl_c\n");
+        if(zahl_c > zahl_a && zahl_c > zahl_b) {
+            printf("%d\n", zahl_c);
 
 
 

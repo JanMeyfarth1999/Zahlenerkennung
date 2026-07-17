712a8964daffd2995d563223f196dc5eb55eaf6b
JanMeyfarth1999 <jan_meyfarth@teilnehmer.cdemy.de>
2026-07-17 08:57:55 +0200

    zwischenspeicher

--- zahlenerkennung.c
+++ zahlenerkennung.c
@@ -7,35 +7,34 @@ int main() {
     
 
         printf("Tippe 3 zahlen ein und lasse bestimmen welches die größte ist: "); 
-        scanf("%d" , &zahl_a);  
-        scanf("%d" , &zahl_b);
-        scanf("%d" , &zahl_c);
+        scanf("%d", &zahl_a);  
+        scanf("%d", &zahl_b);
+        scanf("%d", &zahl_c);
 
+        
 
-        if(zahl_a > zahl_b && zahl_a > zahl_c) {
+        if(zahl_a == zahl_b && zahl_b == zahl_c) {
+            printf("Alle Zahlen sind gleich: %d\n", zahl_a);
+
+        }
+
+        else if(zahl_a > zahl_b && zahl_a > zahl_c) {
             printf("%d\n", zahl_a);
 
         }
                                       
-        if(zahl_b > zahl_a && zahl_b > zahl_c) {
+        else if(zahl_b > zahl_a && zahl_b > zahl_c) {
             printf("%d\n", zahl_b);
 
         }
 
-        if(zahl_c > zahl_a && zahl_c > zahl_b) {
+        else {
             printf("%d\n", zahl_c);
 
-
-
         }
+    
+        return 0;
+    
 
-
-
-
-
-
-
-
-
-
-}
+ 
+    }

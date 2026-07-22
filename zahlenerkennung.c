bdee95beba3b7785e26424b10c0cac031d374652
JanMeyfarth1999 <jan_meyfarth@teilnehmer.cdemy.de>
2026-07-22 09:55:13 +0200

    rdy

--- zahlenerkennung.c
+++ zahlenerkennung.c
@@ -7,34 +7,44 @@ int main() {
     
 
         printf("Tippe 3 zahlen ein und lasse bestimmen welches die größte ist: "); 
-        scanf("%d", &zahl_a);  
-        scanf("%d", &zahl_b);
-        scanf("%d", &zahl_c);
+        scanf("%d" , &zahl_a);  
+        scanf("%d" , &zahl_b);
+        scanf("%d" , &zahl_c);
 
-        
 
-        if (zahl_a == zahl_b && zahl_b == zahl_c) {
-            printf("Alle Zahlen sind gleich: %d\n", zahl_a);
-
-        }
-
-        else if (zahl_a > zahl_b && zahl_a > zahl_c) {
+        if(zahl_a > zahl_b && zahl_a > zahl_c) {
             printf("%d\n", zahl_a);
 
         }
                                       
-        else if (zahl_b > zahl_a && zahl_b > zahl_c) {
+        if(zahl_b > zahl_a && zahl_b > zahl_c) {
             printf("%d\n", zahl_b);
 
         }
 
-        else {
+        if(zahl_c > zahl_a && zahl_c > zahl_b) {
             printf("%d\n", zahl_c);
+        }    
+
+        else  {
+            printf("die Zahlen sind alle gleich\n");
+
+
 
         }
-    
-        return 0;
-    
 
- 
-    }
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+}
